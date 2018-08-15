11a54ae9d4850ca1a878687a295f1a1eb4777558
Roy Jacobs <roy.jacobs@gmail.com>
2018-08-15 13:18:37 +0200

    Add Bulma and FontAwesome along with an admin template

--- blueprint-ui/src/main/frontend/src/app/app.module.ts
+++ blueprint-ui/src/main/frontend/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCoffee);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
