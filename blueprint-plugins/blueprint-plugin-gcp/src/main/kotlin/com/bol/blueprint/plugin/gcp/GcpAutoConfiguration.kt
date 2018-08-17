a81c765490f3abb8b707e7b77408bbf0df03f00a
Roy Jacobs <roy.jacobs@gmail.com>
2018-08-17 14:57:10 +0200

    Removed spurious conditional

--- blueprint-plugins/blueprint-plugin-gcp/src/main/kotlin/com/bol/blueprint/plugin/gcp/GcpAutoConfiguration.kt
+++ blueprint-plugins/blueprint-plugin-gcp/src/main/kotlin/com/bol/blueprint/plugin/gcp/GcpAutoConfiguration.kt
@@ -3,14 +3,12 @@ package com.bol.blueprint.plugin.gcp
 import com.bol.blueprint.store.BlobStore
 import com.bol.blueprint.store.EventStore
 import com.google.cloud.storage.Storage
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EnableConfigurationProperties(GcpProperties::class)
-@ConditionalOnClass(Storage::class)
 class GcpAutoConfiguration {
     @Bean
     fun gcpEventStore(): EventStore {
