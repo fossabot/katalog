2dec2ea270fa2943263a195dd263e335aeb5052f
Roy Jacobs <roy.jacobs@gmail.com>
2018-08-28 15:40:32 +0200

    Enable WebFlux security example

--- blueprint-app/src/main/kotlin/com/bol/blueprint/Config.kt
+++ blueprint-app/src/main/kotlin/com/bol/blueprint/Config.kt
@@ -7,10 +7,19 @@ import com.bol.blueprint.store.InMemoryBlobStore
 import com.bol.blueprint.store.InMemoryEventStore
 import org.springframework.beans.factory.FactoryBean
 import org.springframework.beans.factory.ListableBeanFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.server.SecurityWebFilterChain
 
 @Configuration
 @EnableConfigurationProperties(BlueprintConfigurationProperties::class)
@@ -22,4 +31,37 @@ class Config {
     @Bean
     @Primary
     fun blobStoreFactory(beanFactory: ListableBeanFactory): FactoryBean<BlobStore> = fallback(beanFactory) { InMemoryBlobStore() }
+
+    @Configuration
+    @ConditionalOnProperty("blueprint.security.simple.enabled", matchIfMissing = false)
+    @EnableWebFluxSecurity
+    class SecurityFallbackConfiguration {
+        @Bean
+        fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain = http
+            .authorizeExchange()
+            .pathMatchers("/api/**").hasAuthority("ROLE_USER")
+            .anyExchange().permitAll()
+            .and()
+            .httpBasic()
+            .and()
+            .build()
+
+        @Bean
+        fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+        @Bean
+        fun userDetailsService(): ReactiveUserDetailsService {
+            val user = User.withUsername("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build()
+
+            val admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("USER", "ADMIN")
+                .build()
+
+            return MapReactiveUserDetailsService(user, admin)
+        }
+    }
 }
