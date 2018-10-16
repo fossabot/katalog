bdcb5f4754a352b19e4982fbace2abb5e3055198
Roy Jacobs <roy.jacobs@gmail.com>
2018-10-16 16:22:15 +0200

    More UI for browse page

--- blueprint-app/src/main/kotlin/com/bol/blueprint/Config.kt
+++ blueprint-app/src/main/kotlin/com/bol/blueprint/Config.kt
@@ -45,22 +45,6 @@ class Config {
     @Configuration
     @ConditionalOnProperty("blueprint.test-data.enabled")
     class TestDataConfiguration {
-        object TestData {
-            val NS1 = NamespaceKey("ns1")
-            val NS2 = NamespaceKey("ns2")
-            val SCHEMA1_1 = SchemaKey("ns1", "schema1")
-            val SCHEMA1_2 = SchemaKey("ns1", "schema2")
-            val SCHEMA1_3 = SchemaKey("ns1", "schema3")
-            val SCHEMA1_4 = SchemaKey("ns1", "schema4")
-            val SCHEMA1_5 = SchemaKey("ns1", "schema5")
-            val SCHEMA2_1 = SchemaKey("ns2", "schema1")
-            val SCHEMA2_2 = SchemaKey("ns2", "schema2")
-            val VERSION1 = VersionKey("ns1", "schema1", "1.0.0")
-            val VERSION2 = VersionKey("ns1", "schema1", "1.0.1")
-            val ARTIFACT1 = ArtifactKey("ns1", "schema1", "1.0.0", "artifact1.json")
-            val ARTIFACT2 = ArtifactKey("ns1", "schema1", "1.0.0", "artifact2.json")
-        }
-
         @Autowired
         lateinit var commandHandler: CommandHandler
 
@@ -68,19 +52,20 @@ class Config {
         fun init() {
             runBlocking {
                 with(commandHandler) {
-                    createNamespace(TestData.NS1, GroupKey("group1"))
-                    createNamespace(TestData.NS2, GroupKey("group1"))
-                    createSchema(TestData.SCHEMA1_1, SchemaType.default())
-                    createSchema(TestData.SCHEMA1_2, SchemaType.default())
-                    createSchema(TestData.SCHEMA1_3, SchemaType.default())
-                    createSchema(TestData.SCHEMA1_4, SchemaType.default())
-                    createSchema(TestData.SCHEMA1_5, SchemaType.default())
-                    createSchema(TestData.SCHEMA2_1, SchemaType.default())
-                    createSchema(TestData.SCHEMA2_2, SchemaType.default())
-                    createVersion(TestData.VERSION1)
-                    createVersion(TestData.VERSION2)
-                    createArtifact(TestData.ARTIFACT1, MediaType.JSON, byteArrayOf(1, 2, 3))
-                    createArtifact(TestData.ARTIFACT2, MediaType.JSON, byteArrayOf(1, 2, 3))
+                    createNamespace(NamespaceKey("ns1"), GroupKey("group1"))
+                    createNamespace(NamespaceKey("ns2"), GroupKey("group1"))
+                    createNamespace(NamespaceKey("ns3"), GroupKey("group1"))
+                    createSchema(SchemaKey("ns1", "schema1"), SchemaType.default())
+                    createSchema(SchemaKey("ns1", "schema2"), SchemaType.default())
+                    createSchema(SchemaKey("ns1", "schema3"), SchemaType.default())
+                    createSchema(SchemaKey("ns1", "schema4"), SchemaType.default())
+                    createSchema(SchemaKey("ns1", "schema5"), SchemaType.default())
+                    createSchema(SchemaKey("ns2", "schema1"), SchemaType.default())
+                    createSchema(SchemaKey("ns2", "schema2"), SchemaType.default())
+                    createVersion(VersionKey("ns1", "schema1", "1.0.0"))
+                    createVersion(VersionKey("ns1", "schema1", "1.0.1"))
+                    createArtifact(ArtifactKey("ns1", "schema1", "1.0.0", "artifact1.json"), MediaType.JSON, byteArrayOf(1, 2, 3))
+                    createArtifact(ArtifactKey("ns1", "schema1", "1.0.0", "artifact2.json"), MediaType.JSON, byteArrayOf(4, 5, 6))
                 }
             }
         }
