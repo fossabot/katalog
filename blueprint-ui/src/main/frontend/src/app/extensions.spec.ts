a40599a7dcf494ead586779088a1148d8d8548d7
Roy Jacobs <roy.jacobs@gmail.com>
2018-10-24 15:41:54 +0200

    Add first frontend test

---
+++ blueprint-ui/src/main/frontend/src/app/extensions.spec.ts
@@ -0,0 +1,22 @@
+import "./extensions";
+
+describe('Extensions', () => {
+  describe('toMultiMap', () => {
+    it('should map an array to a multimap', () => {
+      const input = [
+        { id: "a", value: "foo" },
+        { id: "a", value: "bar" },
+        { id: "b", value: "baz" }
+      ];
+
+      const actual: Map<String, any[]> = input.toMultiMap(i => i.id);
+      expect(actual.get('a')).toEqual([
+        { id: "a", value: "foo" },
+        { id: "a", value: "bar" }
+      ]);
+      expect(actual.get('b')).toEqual([
+        { id: "b", value: "baz" }
+      ]);
+    });
+  });
+});
