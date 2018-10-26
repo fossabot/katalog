3951686d7ffe200ae456fae911217886cd41a37b
Roy Jacobs <roy.jacobs@gmail.com>
2018-10-26 09:58:29 +0200

    Cleanup breadcrumbs

--- blueprint-ui/src/main/frontend/src/app/breadcrumbs/breadcrumbs.component.ts
+++ blueprint-ui/src/main/frontend/src/app/breadcrumbs/breadcrumbs.component.ts
@@ -17,12 +17,14 @@ export class BreadcrumbsComponent {
     result.push({ text: 'Namespaces', routerLink: ['/namespaces'] });
     if (this.namespace) {
       result.push({ text: this.namespace.namespace, icon: IconNamespace, routerLink: ['/namespaces', this.namespace.namespace] });
-    }
-    if (this.schema) {
-      result.push({ text: this.schema.schema, icon: IconSchema, routerLink: ['/namespaces', this.schema.namespace.namespace, this.schema.schema] });
-    }
-    if (this.version) {
-      result.push({ text: this.version.version, icon: IconVersion, routerLink: ['/namespaces', this.version.id] });
+
+      if (this.schema) {
+        result.push({ text: this.schema.schema, icon: IconSchema, routerLink: ['/namespaces', this.schema.namespace.namespace, this.schema.schema] });
+
+        if (this.version) {
+          result.push({ text: this.version.version, icon: IconVersion, routerLink: ['/namespaces', this.schema.namespace.namespace, this.schema.schema, this.version.id] });
+        }
+      }
     }
 
     // The last breadcrumb is considered active
