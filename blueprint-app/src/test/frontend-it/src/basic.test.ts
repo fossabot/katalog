12db5ebe66d05941efecf5704ee4d15b17f38f4f
Roy Jacobs <roy.jacobs@gmail.com>
2018-10-29 13:25:03 +0100

    Add first integration test

---
+++ blueprint-app/src/test/frontend-it/src/basic.test.ts
@@ -0,0 +1,14 @@
+describe('Basic', () => {
+    beforeAll(async () => {
+        await page.goto('http://localhost:8080');
+    });
+
+    it('can login', async () => {
+        await expect(page).toFillForm('form[name="login"]', {
+            username: 'user',
+            password: 'user',
+        });
+        await expect(page).toClick('button', { text: 'Login' });
+        await expect(page).toMatch('Welcome');
+    });
+});
