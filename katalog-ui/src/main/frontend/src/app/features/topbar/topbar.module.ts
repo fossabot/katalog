30adab8327fc332dedd1524e9bb707d17bb6201a
Roy Jacobs <roy.jacobs@gmail.com>
2018-12-04 14:58:21 +0100

    Use router in topbar, instead of direct link

--- katalog-ui/src/main/frontend/src/app/features/topbar/topbar.module.ts
+++ katalog-ui/src/main/frontend/src/app/features/topbar/topbar.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from "@angular/core";
 import {TopBarComponent} from "~/features/topbar/topbar.component";
 import {ClarityModule} from "@clr/angular";
 import {AuthModule} from "~/shared/auth/auth.module";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   imports: [
+    AuthModule,
     ClarityModule,
-    AuthModule
+    RouterModule
   ],
   declarations: [
     TopBarComponent
