bcf17a03f277ec25a8e51a7d260a7bc6a773ff61
Roy Jacobs <roy.jacobs@gmail.com>
2019-01-29 12:57:01 +0100

    Fix Postgres tests

--- katalog-plugins/katalog-plugin-postgres/src/main/kotlin/com/bol/katalog/plugin/postgres/PostgresAutoConfiguration.kt
+++ katalog-plugins/katalog-plugin-postgres/src/main/kotlin/com/bol/katalog/plugin/postgres/PostgresAutoConfiguration.kt
@@ -4,11 +4,13 @@ import com.bol.katalog.messaging.MessageBus
 import com.bol.katalog.store.BlobStore
 import com.bol.katalog.store.EventStore
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.core.JdbcTemplate
 
 @Configuration
+@EnableConfigurationProperties
 class PostgresAutoConfiguration {
     @Bean
     @ConditionalOnProperty("katalog.event-store.type", havingValue = "postgres")
