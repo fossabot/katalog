f15813bd4a260c654e8097f19f0d17d8050554bc
Roy Jacobs <roy.jacobs@gmail.com>
2019-02-06 11:19:58 +0100

    Remove Atomix code, to focus on Hazelcast

--- katalog-plugins/katalog-plugin-atomix/src/test/kotlin/com/bol/katalog/plugin/atomix/TestApplication.kt
+++
@@ -1,6 +0,0 @@
-package com.bol.katalog.plugin.atomix
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
-@SpringBootApplication
-class TestApplication
--- katalog-plugins/katalog-plugin-atomix/src/test/kotlin/com/bol/katalog/plugin/atomix/TestData.kt
+++
@@ -1,9 +0,0 @@
-package com.bol.katalog.plugin.atomix
-
-import java.time.Clock
-import java.time.Instant
-import java.time.ZoneId
-
-object TestData {
-    val clock: Clock = Clock.fixed(Instant.EPOCH, ZoneId.systemDefault())
-}
